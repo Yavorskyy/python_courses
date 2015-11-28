dcfa253f6ab3b8118161a422c2d20a8ab10c663b
Yavorskyy <andyyavor85@gmail.com>
2015-11-28 12:52:18 +0200

    task11. vertical on extra_branch

---
+++ task11.py
@@ -0,0 +1,12 @@
+from random import randint
+
+data = [randint(1, 20) for _ in xrange(20)]
+print(data)
+m = max(data)
+
+hhisto = [('o' * i).rjust(m) for i in data]
+for line in hhisto:
+    print(line)
+
+for line in zip(*hhisto):
+    print(''.join(line))
